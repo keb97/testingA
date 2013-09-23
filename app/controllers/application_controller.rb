fbd41cf8ee2f447292f7e91df56e83676cd1b38a
keb97 <keb97@cornell.edu>
2013-09-23 11:01:47 -0700

    commiting my work before pulling from techfounder repo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
@@ -19,7 +20,7 @@ class ApplicationController < ActionController::Base
                        when current_user
                          UserAbility.new(current_user)
                        when current_admin
-                         AdminAbility.new
+                         AdminAbility.new(current_admin)
                        when current_company_user
                          CompanyUserAbility.new(current_company_user)
                        else
